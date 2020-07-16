54c14c86cfc902a97bd8838aa2dcd46ee6645f8b
Inigo-santiu <dstavroff@gmail.com>
2020-07-16 20:11:58 +0200

    emails properly displayed, only names needed

--- lib/mayoral_mail_list.rb
+++ lib/mayoral_mail_list.rb
@@ -2,5 +2,36 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-def mailing_list
-  page = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/'))
+def get_townhall_email(townhall_url)
+  email = Nokogiri::HTML(URI.open(townhall_url))
+  email_array = []
+  
+  email.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |avernes| 
+    email_array << avernes.text
+    print email_array
+  end
+end
+
+def get_townhall_urls
+  mayoral_page = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
+  valdoise_array = []
+  mayoral_page.xpath('//p/a/@href').each do |villes|
+    villes_url = "https://www.annuaire-des-mairies.com" + villes.text.delete_prefix(".")
+    valdoise_array << villes_url
+  end
+  mayoral_page.xpath()
+  presentation = Hash[???.zip(valdoise_array)]
+
+
+
+  return valdoise_array
+end
+
+def execute
+  get_townhall_urls
+  get_townhall_urls.each do |url|
+  get_townhall_email(url)
+  end
+end
+
+execute
